a1da0e515963d4ad77dd50b589f665280af6f4f5
Rutesh Patil <ruteshp21@gmail.com>
2021-02-20 21:07:51 +0530

    Added review-service generated via Spring Initializr

---
+++ microservices/review-service/src/test/java/com/rutesh/microservices/review/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rutesh.microservices.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
