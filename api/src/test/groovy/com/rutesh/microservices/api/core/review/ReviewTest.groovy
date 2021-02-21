a4aca5241b8f6073f1741f52881d6ebbc7657fb8
Rutesh Patil <ruteshp21@gmail.com>
2021-02-21 17:28:33 +0530

    Updating unit test cases package structure

---
+++ api/src/test/groovy/com/rutesh/microservices/api/core/review/ReviewTest.groovy
@@ -0,0 +1,16 @@
+package com.rutesh.microservices.api.core.review
+
+import spock.lang.Specification
+
+class ReviewTest extends Specification {
+    /*def "someLibraryMethod returns true"() {
+        setup:
+        def lib = new Library()
+
+        when:
+        def result = lib.someLibraryMethod()
+
+        then:
+        result == true
+    }*/
+}
