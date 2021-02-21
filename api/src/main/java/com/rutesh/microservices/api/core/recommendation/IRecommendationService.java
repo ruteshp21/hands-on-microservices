7429c26e13a895992efa5f2eff315d826beea9a7
Rutesh Patil <ruteshp21@gmail.com>
2021-02-21 13:56:48 +0530

    Update IRecommendationService.java and Recommendation.java

--- api/src/main/java/com/rutesh/microservices/api/core/recommendation/IRecommendationService.java
+++ api/src/main/java/com/rutesh/microservices/api/core/recommendation/IRecommendationService.java
@@ -1,4 +1,9 @@
 package com.rutesh.microservices.api.core.recommendation;
 
+import java.util.List;
+
 public interface IRecommendationService {
+
+    List<Recommendation> getRecommendations(int productId);
+
 }
--- api/src/main/java/com/rutesh/microservices/api/core/recommendation/Recommendation.java
+++ api/src/main/java/com/rutesh/microservices/api/core/recommendation/Recommendation.java
@@ -1,4 +1,47 @@
 package com.rutesh.microservices.api.core.recommendation;
 
 public class Recommendation {
+
+    private final int recommendationId;
+    private final int productId;
+    private final String author;
+    private final int rate;
+    private final String content;
+
+    public Recommendation() {
+        this.recommendationId = 0;
+        this.productId = 0;
+        this.author = "";
+        this.rate = 0;
+        this.content = "";
+    }
+
+    public Recommendation(int recommendationId, int productId, String author, int rate, String content) {
+        this.recommendationId = recommendationId;
+        this.productId = productId;
+        this.author = author;
+        this.rate = rate;
+        this.content = content;
+    }
+
+    public int getRecommendationId() {
+        return recommendationId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
 }
