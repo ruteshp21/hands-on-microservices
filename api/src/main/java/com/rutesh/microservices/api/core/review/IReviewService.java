da4b1e20f655859d03aab5be8b1c6b459d804dab
Rutesh Patil <ruteshp21@gmail.com>
2021-02-21 13:56:51 +0530

    Update IReviewService.java and Review.java

--- api/src/main/java/com/rutesh/microservices/api/core/review/IReviewService.java
+++ api/src/main/java/com/rutesh/microservices/api/core/review/IReviewService.java
@@ -1,4 +1,9 @@
 package com.rutesh.microservices.api.core.review;
 
+import java.util.List;
+
 public interface IReviewService {
+
+    List<Review> getReviews(int productId);
+
 }
--- api/src/main/java/com/rutesh/microservices/api/core/review/Review.java
+++ api/src/main/java/com/rutesh/microservices/api/core/review/Review.java
@@ -1,4 +1,46 @@
 package com.rutesh.microservices.api.core.review;
 
 public class Review {
+
+    private final int productId;
+    private final int reviewId;
+    private final String author;
+    private final String subject;
+    private final String content;
+
+    public Review() {
+        this.productId = 0;
+        this.reviewId = 0;
+        this.author = "";
+        this.subject = "";
+        this.content = "";
+    }
+
+    public Review(int productId, int reviewId, String author, String subject, String content) {
+        this.productId = productId;
+        this.reviewId = reviewId;
+        this.author = author;
+        this.subject = subject;
+        this.content = content;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
