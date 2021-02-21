850a1f136e2f29f7f41991385e6cc1af9e0b8a15
Rutesh Patil <ruteshp21@gmail.com>
2021-02-21 13:56:43 +0530

    Update IProductService.java and Product.java

--- api/src/main/java/com/rutesh/microservices/api/core/product/IProductService.java
+++ api/src/main/java/com/rutesh/microservices/api/core/product/IProductService.java
@@ -1,4 +1,7 @@
 package com.rutesh.microservices.api.core.product;
 
 public interface IProductService {
+
+    Product getProduct(int productId);
+
 }
--- api/src/main/java/com/rutesh/microservices/api/core/product/Product.java
+++ api/src/main/java/com/rutesh/microservices/api/core/product/Product.java
@@ -1,4 +1,33 @@
 package com.rutesh.microservices.api.core.product;
 
 public class Product {
+
+    private final int productId;
+    private final String name;
+    private final int weight;
+
+    public Product() {
+        this.productId = 0;
+        this.name = "";
+        this.weight = 0;
+    }
+
+    public Product(int productId, String name, int weight) {
+        this.productId = productId;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
 }
